0dd6ed6f5d6ffc36ffbcd78b955d66256e602f70
kenzostore <90239401+kenzostore@users.noreply.github.com>
2021-11-29 21:42:44 +0700

    Initial commit

---
+++ shop/allpayment.js
@@ -0,0 +1,35 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY KENZ FC
+
+*🏧 BANK*
+
+1. BRI 
+	ㅁ Ke Owner Aja
+2. BCA 
+	ㅁ Ke Owner Aja
+3. BNI 
+	ㅁ Ke Owner Aja
+4. PERMATA
+	ㅁ Ke Owner Aja
+	
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ Ke Owner Aja
+2. OVO
+	ㅁ Ke Owner Aja
+3. DANA
+	ㅁ Ke Owner Aja
+	
+Semuanya atas nama Di Owner Ja
+
+*📲 PULSA*
+
+Tidak tersedia
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
---
+++ shop/donasi.js
@@ -0,0 +1,29 @@
+const donasibot = () => {
+return `DONASI BISA VIA BANK / E-MONEY
+
+*🏧 BANK*
+
+1. BRI 
+	ㅁ Owner
+2. BCA 
+	ㅁ Owner
+3. BNI 
+	ㅁ Owner
+4. PERMATA
+	ㅁ Owner
+	
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ Owner
+2. OVO
+	ㅁ Owner
+3. DANA
+	ㅁ Owner
+	
+Semuanya atas nama KenzStore
+`
+	}
+
+exports.donasibot = donasibot
+ 
